818ee748ff971b23e93f9826edcc8aebf45169ca
dhruvikparmar <dhruvikparmar1008@gmail.com>
2024-11-20 11:38:51 +0530

    hhh

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+
+    cout<<"Enter Value Of A:"<<"\n";
+    cin>>a;
+    cout<<"Enter Value Of B:"<<"\n";
+    cin>>b;
+
+    cout<<a+b<<"\n";
+    cout<<a-b<<"\n";
+    cout<<a/b<<"\n";
+    cout<<a*b<<"\n";
+    cout<<a%b<<"\n";
+
+    return 0;
+}
